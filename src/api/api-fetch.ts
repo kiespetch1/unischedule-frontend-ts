a270678d2ae7377b5274d3efb9138e5c3d321209
Тараненко Данил <kiespetch@ya.ru>
2025-05-15 12:15:17 +0300

    обновлены зависимости, деактивирована xsrf защита

--- src/api/api-fetch.ts
+++ src/api/api-fetch.ts
@@ -1,14 +1,17 @@
 import { ApiError } from "./api-error"
-import { getXsrfToken, setXsrfToken } from "@/features/auth/utils/tokens"
+import {
+  getXsrfToken,
+  //  setXsrfToken
+} from "@/features/auth/utils/tokens"
 import { refresh } from "@/features/auth/refresh"
-import { getAntiforgeryRefreshUrl, getServiceTypeFromUrl } from "@/api/urls.ts"
+// import { getAntiforgeryRefreshUrl, getServiceTypeFromUrl } from "@/api/urls.ts"
 
 let refreshPromise: Promise<void> | null = null
-const xsrfRenewPromises: Record<string, Promise<void> | null> = {
-  identity: null,
-  schedule: null,
-  events: null,
-}
+// const xsrfRenewPromises: Record<string, Promise<void> | null> = {
+//   identity: null,
+//   schedule: null,
+//   events: null,
+// }
 
 export async function apiFetch(
   input: RequestInfo,
@@ -54,27 +57,27 @@ export async function apiFetch(
     }
 
     // 403 -> пробуем получить новый request‑токен с нужного сервиса и повторить
-    if (response.status === 403 && useXsrfProtection && tries < 2) {
-      const url = typeof input === "string" ? input : input.url
-      const serviceType = getServiceTypeFromUrl(url)
-
-      if (!xsrfRenewPromises[serviceType]) {
-        const antiforgeryUrl = getAntiforgeryRefreshUrl(serviceType)
-        xsrfRenewPromises[serviceType] = fetch(antiforgeryUrl, { credentials: "include" })
-          .then(r => setXsrfToken(r.headers.get("XSRF-TOKEN") || ""))
-          .finally(() => (xsrfRenewPromises[serviceType] = null))
-      }
-      await xsrfRenewPromises[serviceType]
-      return doFetch(tries + 1)
-    }
+    // if (response.status === 403 && useXsrfProtection && tries < 2) {
+    //   const url = typeof input === "string" ? input : input.url
+    //   const serviceType = getServiceTypeFromUrl(url)
+    //
+    //   if (!xsrfRenewPromises[serviceType]) {
+    //     const antiforgeryUrl = getAntiforgeryRefreshUrl(serviceType)
+    //     xsrfRenewPromises[serviceType] = fetch(antiforgeryUrl, { credentials: "include" })
+    //       .then(r => setXsrfToken(r.headers.get("XSRF-TOKEN") || ""))
+    //       .finally(() => (xsrfRenewPromises[serviceType] = null))
+    //   }
+    //   await xsrfRenewPromises[serviceType]
+    //   return doFetch(tries + 1)
+    // }
 
     if (!response.ok) {
       const data = await response.json().catch(() => null)
       throw new ApiError(response, data)
     }
 
-    const newXsrf = response.headers.get("XSRF-TOKEN")
-    if (newXsrf) setXsrfToken(newXsrf)
+    // const newXsrf = response.headers.get("XSRF-TOKEN")
+    // if (newXsrf) setXsrfToken(newXsrf)
 
     return response
   }
